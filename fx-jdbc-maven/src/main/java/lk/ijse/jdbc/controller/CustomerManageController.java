1281d537a112ebf5f3fbc21b81d890ad2ddc9645
danu <danujagreru@gmail.com>
2023-03-07 16:25:07 +0530

    fx open

---
+++ fx-jdbc-maven/src/main/java/lk/ijse/jdbc/controller/CustomerManageController.java
@@ -0,0 +1,29 @@
+package lk.ijse.jdbc.controller;
+
+/*
+    @author DanujaV
+    @created 3/7/23 - 3:48 PM   
+*/
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class CustomerManageController {
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+}
