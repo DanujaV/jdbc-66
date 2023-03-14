13b016ab8b142ed2362bf1903edea103c41cac0e
danu <danujagreru@gmail.com>
2023-03-14 15:24:34 +0530

    mvc design pattern

--- 04-thogakade/src/main/java/lk/ijse/thogakade/controller/ItemFormController.java
+++ 04-thogakade/src/main/java/lk/ijse/thogakade/controller/ItemFormController.java
@@ -10,12 +10,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lk.ijse.thogakade.model.ItemModel;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class ItemFormController {
     @FXML
@@ -68,12 +71,35 @@ public class ItemFormController {
 
     @FXML
     void btnSaveOnAction(ActionEvent event) {
+        String code = txtCode.getText();
+        String description = txtDescription.getText();
+        double unitPrice = Double.parseDouble(txtUnitPrice.getText());
+        int qtyOnHand = Integer.parseInt(txtQtyOnHand.getText());
 
+        try {
+            boolean isSaved = ItemModel.save(code, description, unitPrice, qtyOnHand);
+            if(isSaved) {
+                new Alert(Alert.AlertType.CONFIRMATION, "Item saved!").show();
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, "something went wrong!").show();
+        }
     }
 
     @FXML
     void btnUpdateOnAction(ActionEvent event) {
+        String code = txtCode.getText();
+        String description = txtDescription.getText();
+        double unitPrice = Double.parseDouble(txtUnitPrice.getText());
+        int qtyOnHand = Integer.parseInt(txtQtyOnHand.getText());
 
+        try {
+            boolean isUpdated = ItemModel.update(code, description, unitPrice, qtyOnHand);
+            new Alert(Alert.AlertType.CONFIRMATION, "Item updated!").show();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "something went wrong!").show();
+        }
     }
 
     @FXML
---
+++ 04-thogakade/src/main/java/lk/ijse/thogakade/model/ItemModel.java
@@ -0,0 +1,55 @@
+package lk.ijse.thogakade.model;
+
+/*
+    @author DanujaV
+    @created 3/14/23 - 2:27 PM   
+*/
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ItemModel {
+    private static final String URL = "jdbc:mysql://localhost:3306/ThogaKade";
+    private static final Properties props = new Properties();
+
+    static {
+        props.setProperty("user", "root");
+        props.setProperty("password", "Danu25412541@");
+    }
+    public static boolean save(String code, String description, double unitPrice, int qtyOnHand) throws SQLException {
+        try (Connection con = DriverManager.getConnection(URL, props)) {
+            String sql = "INSERT INTO Item(code, description, unitPrice, qtyOnHand) " +
+                    "VALUES(?, ?, ?, ?)";
+
+            PreparedStatement pstm = con.prepareStatement(sql);
+            pstm.setString(1, code);
+            pstm.setString(2, description);
+            pstm.setDouble(3, unitPrice);
+            pstm.setInt(4, qtyOnHand);
+
+            int affectedRows = pstm.executeUpdate();
+            if(affectedRows > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public static boolean update(String code, String description, double unitPrice, int qtyOnHand) throws SQLException {
+        try (Connection con = DriverManager.getConnection(URL, props)) {
+            String sql = "UPDATE Item SET description = ?, unitPrice = ?, " +
+                    "qtyOnHand = ? WHERE code = ?";
+            PreparedStatement pstm = con.prepareStatement(sql);
+            pstm.setString(1, description);
+            pstm.setDouble(2, unitPrice);
+            pstm.setInt(3, qtyOnHand);
+            pstm.setString(4, code);
+
+            return pstm.executeUpdate() > 0;
+        }
+    }
+}
